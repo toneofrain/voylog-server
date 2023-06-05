4dbc3e63a44872a0e1ee580319ba2279205f7aec
toneofrain <toneofrain@gmail.com>
2023-06-05 19:55:10 +0900

    chore: initiate project

---
+++ src/main/java/dev/saintho/voylog/VoylogApplication.java
@@ -0,0 +1,13 @@
+package dev.saintho.voylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoylogApplication.class, args);
+	}
+
+}
