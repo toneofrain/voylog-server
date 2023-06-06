d3508b9bf1387670de70a3637e42a25613242273
조성호 saintho <saintho95@gmail.com>
2023-06-06 14:51:36 +0900

    Feat/seongho/1 api details (#2)
    * chore: Spring Rest Docs Swagger UI 연동 (#01)
    
    * chore: Spring Rest Docs Swagger UI 연동 (#01)

---
+++ src/main/java/dev/saintho/voylog/index/IndexController.java
@@ -0,0 +1,16 @@
+package dev.saintho.voylog.index;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/index")
+public class IndexController {
+
+	@GetMapping
+	public ResponseEntity<String> hello() {
+		return ResponseEntity.ok("Hello!");
+	}
+}
